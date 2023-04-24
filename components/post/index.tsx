01a5461e02d9f7740449bc5ab0f0556d4ebc5d80
Saveliy <sava2698@gmail.com>
2023-04-24 15:00:57 +0600

    Posts section finished

--- components/post/index.tsx
+++ components/post/index.tsx
@@ -8,12 +8,12 @@ import React from 'react';
 
 export const Post: React.FC = () => {
   return (
-    <Paper elevation={0} className="p-20">
+    <Paper elevation={0} className="p-20" classes={{ root: styles.paper }}>
       <Typography variant="h5" className={styles.title}>
         Кот прилег отдохнуть в английском парке миниатюр - и стал героем шуток и
         фотожаб о "гигантском пушистом захватчике
       </Typography>
-      <Typography className="mt-15 mb-15">
+      <Typography className="mt-10 mb-15">
         Пока одни не могли соотнести размеры животного и окружения, другие
         начали создавать апокалиптические сюжеты с котом в главной роли.
       </Typography>
